f03851025b2981ecb57f206fbd27700ab9b36a4d
jensenjamison <jensenjamison@yahoo.com>
2019-08-22 13:44:50 -0500

    breaking bad api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+constructor(props){
+  super();
+
+  this.state = {
+    characters: []
+  }
+  this.getCharacters = this.getCharacters.bind(this)
+}
+
+
+  getCharacters() {
+   const promise = axios.get("https://www.breakingbadapi.com/api/characters")
+   promise.then(response => {
+     console.log(response.data)
+     this.setState({characters: response.data })
+   })
+  }
+  
+  
+  render(){
+    return (
+      <div className="App">
+      <h1>The Breaking Bad Api</h1>
+      <button onClick={this.getCharacters}>Show Characters</button>
+      {this.state.characters.map(character => (
+        <div key={character.char_id}>
+          <img src={character.img} style={{ maxWidth: "100px" }} />
+          <h2>{character.name}</h2>
+          <h3>{character.nickname}</h3>
+        </div>
+      ))}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
